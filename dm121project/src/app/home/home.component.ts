3ff8f0cb763b5d6258917a147c73ab65f7a827d6
eltonbarbosa <eltonbr.barbosa@gmail.com>
2020-07-16 20:03:16 -0300

    Remove unsed imports

--- dm121project/src/app/home/home.component.ts
+++ dm121project/src/app/home/home.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-<<<<<<< HEAD
-=======
-import { Observable } from 'rxjs';
-
-import { AngularFireDatabase } from '@angular/fire/database'
->>>>>>> fbab89d027d67d419cd520d603325aa91f2a7207
 
 @Component({
   selector: 'app-home',
