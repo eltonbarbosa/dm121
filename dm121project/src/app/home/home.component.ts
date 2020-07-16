fbab89d027d67d419cd520d603325aa91f2a7207
eltonbarbosa <eltonbr.barbosa@gmail.com>
2020-07-16 19:44:28 -0300

    Add base structure for dm121 project

---
+++ dm121project/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { AngularFireDatabase } from '@angular/fire/database'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  newsLetterForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+   this.newsLetterForm = this.fb.group({
+    email: ['', Validators.email]
+   });
+  }
+
+  onSubmit() {}
+
+}
